3d889934281f4af2dbc0cdf3e1d59d538bcdd329
LegendHeros1 <jinpeak2@gmail.com>
2019-04-28 18:10:38 -0500

    Version 5: working api calls

--- app/src/main/java/cs125/jeopardy/gameActivity.java
+++ app/src/main/java/cs125/jeopardy/gameActivity.java
@@ -13,24 +13,30 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Random;
+
 
 public class gameActivity extends AppCompatActivity {
-    /** Default logging tag for messages from the main activity. */
-    private static final String TAG = "Jeopardy:gameActivity";
-    private TextView mTextViewResult;
-    private RequestQueue mQueue;
 
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+    private static String display = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up the queue for our API requests
+        requestQueue = Volley.newRequestQueue(this);
+
         setContentView(R.layout.activity_game);
 
         //get information from intent and set name for player 1 and player 2
@@ -40,7 +46,7 @@ public class gameActivity extends AppCompatActivity {
         player1_name.setText(intent.getStringExtra("player1"));
         player2_name.setText(intent.getStringExtra("player2"));
 
-        TextView mTextViewResult = findViewById(R.id.testing_box);
+        final TextView mTextViewResult = findViewById(R.id.testing_box);
         int[] questionButtonsID = {R.id.point_200_1, R.id.point_200_2, R.id.point_200_3, R.id.point_200_4,
                 R.id.point_400_1, R.id.point_400_2, R.id.point_400_3, R.id.point_400_4,
                 R.id.point_600_1, R.id.point_600_2, R.id.point_600_3, R.id.point_600_4,
@@ -52,7 +58,8 @@ public class gameActivity extends AppCompatActivity {
             questionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    trivalApiCall();
+                    trivalAPICall("https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple");
+                    mTextViewResult.setText(display);
                     //openQuestionActivity();
                 }
             });
@@ -60,42 +67,113 @@ public class gameActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * @param category name of the category
+     * Category IDs
+     * general knowledge: 9
+     * vehicles: 28
+     *
+     * entertainment: 10 - 16, 29, 31 - 32
+     * mythology: 20
+     * art: 25
+     *
+     * science: 17 - 19, 30
+     * animals: 27
+     *
+     * Geography: 22
+     * History: 23
+     * Politics: 24
+     *
+     *
+     * @param difficult difficulty level - easy, medium, hard
+     * @return something like this https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple
+     */
+
+    public String getAPIurl(String category, String difficult) {
+        String baseUrl = "https://opentdb.com/api.php?amount=1&";
+        String categoryPart = "category="; //need to include id
+        String difficultyPart = "&difficulty=" + difficult;
+        String questionType = "&type=multiple";
+
+        int[] category1_ids = new int[] {17, 18, 19, 30};
+        int[] category2_ids = new int[] {22, 23, 24};
+        int[] category3_ids = new int[] {10, 11, 12, 13, 14, 15, 16, 29, 31, 32};
+        int[] category4_ids = new int[] {9, 28};
+
+        //randomly pick a id from the respective category array
+        Random random = new Random();
+        int randomNumber;
+        switch (category) {
+            case "category1":
+                //pick a number between 0 and 3 inclusive
+                randomNumber = random.nextInt(category1_ids.length);
+                categoryPart += category1_ids[randomNumber];
+                break;
+            case "category2":
+                //pick a number between 0 and 2 inclusive
+                randomNumber = random.nextInt(category2_ids.length);
+                categoryPart += category2_ids[randomNumber];
+                break;
+            case "category3":
+                //pick a number between 0 and 9 inclusive
+                randomNumber = random.nextInt(category3_ids.length);
+                categoryPart += category3_ids[randomNumber];
+                break;
+            case "category4":
+                //pick a number between 0 and 1 inclusive
+                randomNumber = random.nextInt(category4_ids.length);
+                categoryPart += category4_ids[randomNumber];
+                break;
+             //hopefully doesn't run
+            default:
+                categoryPart += 9;
+                break;
+        }
+        return baseUrl + categoryPart + difficultyPart + questionType;
+
+
+    }
+
     void openQuestionActivity() {
-        trivalApiCall();
+        //trivalApiCall();
         Intent intent = new Intent(this, questionActivity.class);
         startActivity(intent);
         finish();
     }
 
-    private void trivalApiCall() {
-        String url = "https://opentdb.com/api.php?amount=10&type=multiple";
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONArray result = response.getJSONArray("results");
-                    JSONObject firstSet = result.getJSONObject(0);
-
-                    //get question, answer, incorrect multiple choice
-                    String question = firstSet.getString("question");
-                    String correctAnswer = firstSet.getString("correct_answer");
-                    JSONArray incorrectAnswers = firstSet.getJSONArray("incorrect_answers");
-                    mTextViewResult.append(question + " " + correctAnswer);
-                    Log.d(TAG, "WORKING");
-                    Log.d(TAG, question + " " + correctAnswer);
-
-                } catch (JSONException e) {
-                    mTextViewResult.append("Not working!");
-                    Log.d(TAG, "NOT WORKING");
-                    e.printStackTrace();
+    /**
+     * attempt to call api with the url
+     * @param url trivial category url
+     */
+    void trivalAPICall(String url) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    parseJson(response);
                 }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    //ignore
+                }
+            });
+            jsonObjectRequest.setShouldCache(false);
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void parseJson(JSONObject response) {
+        try {
+            JSONArray results = response.getJSONArray("results");
+            JSONObject firstQuestionSet = results.getJSONObject(0);
+
+            String question = firstQuestionSet.getString("question");
+            display = question;
+        } catch (JSONException ignored) {
+            //do nothing
+        }
     }
 }
